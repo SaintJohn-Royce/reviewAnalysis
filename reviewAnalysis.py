d14e722c315191d0d7512b76ffb674a044acae0f
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-07 13:04:30 +0800

    filter line shorter than 100

--- reviewAnalysis.py
+++ reviewAnalysis.py
@@ -4,6 +4,10 @@ totalNumber = 0
 
 lenLine = 0
 
+lineShorter100 = 0
+
+dataShorter100 = []
+
 with open('reviews.txt', 'r') as file:
 
 	for line in file:
@@ -12,9 +16,19 @@ with open('reviews.txt', 'r') as file:
 
 		totalNumber = lenLine + totalNumber
 
-		data.append(lenLine)
+		data.append(line)
+
+		if lenLine < 100:
+
+			dataShorter100.append(line)
+
+			lineShorter100 = lineShorter100 + 1
 
 avgNum = totalNumber / len(data)
 
 # average number of characters per comment
-print(avgNum)
+print('the average number of characters per line of comment is', avgNum)
+
+print('the number of comments shorter than 100 characters is', lineShorter100)
+
+print(dataShorter100[0])
