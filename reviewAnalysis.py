f48418ed4f1faac5d32c6df8602dc986de20d536
Saint-John Royce <vzhu6682@uni.sydney.edu.au>
2020-02-07 13:35:14 +0800

    filter good analysis

--- reviewAnalysis.py
+++ reviewAnalysis.py
@@ -1,12 +1,11 @@
 data = []
-
 totalNumber = 0
-
 lenLine = 0
 
 lineShorter100 = 0
-
 dataShorter100 = []
+lineGood = 0
+dataGood = []
 
 with open('reviews.txt', 'r') as file:
 
@@ -18,11 +17,21 @@ with open('reviews.txt', 'r') as file:
 
 		data.append(line)
 
+		# find the lines shorter than 100 characters
+
 		if lenLine < 100:
 
 			dataShorter100.append(line)
 
 			lineShorter100 = lineShorter100 + 1
+
+		# find the lines with good in them
+
+		if 'good' in line:
+
+			dataGood.append(line)
+
+			lineGood = lineGood + 1
 
 avgNum = totalNumber / len(data)
 
@@ -31,4 +40,4 @@ print('the average number of characters per line of comment is', avgNum)
 
 print('the number of comments shorter than 100 characters is', lineShorter100)
 
-print(dataShorter100[0])
+print('the number of comments with the word "good" in them is', lineGood)
